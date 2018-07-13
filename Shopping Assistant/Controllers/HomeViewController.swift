5534adf95888fb108ca575a381917d3d38c042dd
Tom Bell <tom.bell.code@gmail.com>
2018-07-13 20:57:06 +0100

    Add empty shopping list view and embed home view in navigation controller

--- Shopping Assistant/Controllers/HomeViewController.swift
+++ Shopping Assistant/Controllers/HomeViewController.swift
@@ -9,11 +9,7 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func showShoppingList() {
+        performSegue(withIdentifier: "ShowShoppingList", sender: self)
     }
 }
