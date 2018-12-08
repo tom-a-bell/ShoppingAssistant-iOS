8e02eabf551dfdd7d0f77d5f3edf7515af36adf9
Tom Bell <tom.bell.code@gmail.com>
2018-12-08 14:23:04 +0000

    Add Crashlytics as Carthage dependency to enable crash reporting

--- Shopping Assistant/AppDelegate.swift
+++ Shopping Assistant/AppDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Fabric
+import Crashlytics
 import CoreLocation
 
 @UIApplicationMain
@@ -7,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         Log.configure()
 
         GoogleMapsService.shared.initialize()
